e10c31a43970b834e0ca4fb2aabef0ee6f24735d
Noah Michél <noahmichel999@gmail.com>
2025-03-07 19:45:44 -0500

    Create page.tsx
    sent messages page incorperation

---
+++ app/sent/page.tsx
@@ -0,0 +1,8 @@
+export default function SentMessagesPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">Sent Messages </h1>
+            <p>Here are your latest sent messages.</p>
+        </div>
+    );
+}
