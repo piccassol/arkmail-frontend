010d129cd33a01d8267c82bb974529b36887ff9a
Noah Michél <noahmichel999@gmail.com>
2025-03-07 19:44:12 -0500

    Create page.tsx

---
+++ app/newsletters/page.tsx
@@ -0,0 +1,8 @@
+export default function NewslettersPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">Newsletters</h1>
+            <p>Here are your latest newsletters.</p>
+        </div>
+    );
+}
