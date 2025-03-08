cf477b5ebc6cda255db55dd8328100e42f8e9caf
Noah Michél <noahmichel999@gmail.com>
2025-03-07 19:01:59 -0500

    Create page.tsx
    create inbox

---
+++ app/inbox/page.tsx
@@ -0,0 +1,8 @@
+export default function InboxPage() {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">Inbox</h1>
+            <p>Here are your latest emails.</p>
+        </div>
+    );
+}
